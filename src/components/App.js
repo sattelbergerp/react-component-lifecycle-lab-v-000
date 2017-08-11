83597e1a973b5779819b19103900216087dbcf95
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-08-11 17:02:54 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,21 @@ class App extends React.Component {
 
   constructor() {
     super();
-    
+
     this.state = {
       latestTweets: []
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: componentDidMount()
-  // TODO: componentWillUnmount()
+  componentWillMount(){
+    this.fetchTweets();
+  }
+  componentDidMount(){
+    this.startInterval();
+  }
+  componentWillUnmount(){
+    this.cleanUpInterval();
+  }
 
   startInterval = () => {
     this.interval = setInterval(this.fetchTweets, 2000);
